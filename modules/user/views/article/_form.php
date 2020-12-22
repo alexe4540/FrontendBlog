dfa9f75c8a0671018073283405f3805275036691
Ihor Novokhatko <hohohosanta53@gmail.com>
2020-12-22 19:41:48 +0200

    Ihor's changes part 2

---
+++ modules/user/views/article/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Article */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="article-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'date')->textInput() ?>
+
+    <?= $form->field($model, 'topic_id')->dropDownList($topics) ?>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
